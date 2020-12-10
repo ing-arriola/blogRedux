4d95b75c630ec8ab22185eafbf0cbdd32c21f0df
ing-arriola <ingeniero.arriola@gmail.com>
2020-12-10 15:57:22 -0600

    Using redux-thunk middleware to take care of asynchronous code when calling the jsonPlaceholder API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import App from "./Components/App";
 import reducers from "./Reducers";
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
